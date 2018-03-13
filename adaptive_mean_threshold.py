93d6463aef95879199ce1c196aac97a5478f8360
dulajsan <dulaj.san@gmail.com>
2018-03-13 07:21:18 +0530

    adaptive mean threshold

---
+++ adaptive_mean_threshold.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+import numpy as np
+
+
+img=cv.imread('OpenCV_Logo.png',0)
+thresh=cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,2)
+
+cv.imshow('adaptive threshold',thresh)
+cv.waitKey(0)
+cv.destroyAllWindows();
